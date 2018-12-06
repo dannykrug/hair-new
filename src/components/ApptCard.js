9fd8776e5353241b6ea7aed1e599c78c38c06997
dannykrug <dannykrug@gmail.com>
2018-12-06 11:04:42 -0500

    date and time formatted

--- src/components/ApptCard.js
+++ src/components/ApptCard.js
@@ -6,6 +6,18 @@ import ApptDetailPage from './ApptDetailPage'
 
 // const FormattedDate = ReactIntl.FormattedDate
 
+function formatDate(date) {
+  return (date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear()
+}
+
+function formatTime(time){
+  let hours = time.getHours()
+  let minutes = time.getMinutes()
+  hours = hours + 5
+  minutes = minutes < 10 ? '0' + minutes : minutes
+  return hours + ':' + minutes
+}
+
 export default class ApptCard extends Component {
 
   // ApptPage = () => {
@@ -14,15 +26,18 @@ export default class ApptCard extends Component {
   // )
   // }
 
+  date = new Date(this.props.appointment.appointment.date)
+  time = new Date(this.props.appointment.appointment.time)
+
   render(){
-    console.log('appt card props',this.props.appointment.appointment);
+    console.log('date', formatTime(this.time));
     return(
       <div className='appointment-card'>
         <Link to={`/appointments/${this.props.appointment.appointment.id}`}>
           <div>
           <h4>{this.props.appointment.appointment.first_name} {this.props.appointment.appointment.last_name}</h4>
-          <p>{this.props.appointment.appointment.date}</p>
-          <p>{this.props.appointment.appointment.time}</p>
+          <p>{formatDate(this.date)}</p>
+          <p>{formatTime(this.time)}</p>
           </div>
         </Link>
       </div>
--- src/components/ApptDetails.js
+++ src/components/ApptDetails.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react'
 import './Login.css'
 
+function formatDate(date) {
+  return (date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear()
+}
+
+function formatTime(time){
+  let hours = time.getHours()
+  let minutes = time.getMinutes()
+  hours = hours + 5
+  minutes = minutes < 10 ? '0' + minutes : minutes
+  return hours + ':' + minutes
+}
+
 class ApptDetails extends Component {
+
+  date = new Date(this.props.appointment.appointment.date)
+  time = new Date(this.props.appointment.appointment.time)
+
   render() {
     console.log(this.props);
     return(
@@ -10,8 +26,8 @@ class ApptDetails extends Component {
       <div className='stylist'>
       <div className='signup-fields'>
       <h4>{this.props.appointment.appointment.first_name} {this.props.appointment.appointment.last_name}</h4>
-      <p>Date: {this.props.appointment.appointment.date}</p>
-      <p>Time: {this.props.appointment.appointment.time}</p>
+      <p>Date: {formatDate(this.date)}</p>
+      <p>Time: {formatTime(this.time)}</p>
       <p>Service: {this.props.appointment.appointment.service}</p>
       <img className='picture' src={this.props.appointment.appointment.inspo_pic} alt=''/>
       </div>
